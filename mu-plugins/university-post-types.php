e86eec649dbc2adbb8fbca0a5ccd62410d796d09
barry786-dev <mbrsyr@yahoo.com>
2024-10-04 13:27:28 +0200

    last change before using .env

--- mu-plugins/university-post-types.php
+++ mu-plugins/university-post-types.php
@@ -1,6 +1,33 @@
 <?php
 function university_post_types()
 {
+  // Campus post type
+  register_post_type('campus', array(
+    'show_in_rest' => 'true', // to use the modern block editor in wp
+    'supports' => array(
+      'title',
+      'editor',
+      'excerpt', //'custom-fields'
+    ),
+    'rewrite' => array('slug' => 'campuses'),
+    'has_archive' => true, // tell wp that this type of posts has archive url post
+    'public' => true, // this will make the post type visible
+    'menu_icon' => 'dashicons-location-alt', // add icon to this post type
+    'labels' => array( // add info about this post type
+      'name' => 'Campuses', // give the new post type a name
+      'add_new' => 'Add New Campus',
+      'add_new_item' => 'Add New Campus', // give name to add new Item
+      'edit_item' => 'Edit Campuses', // add the name of our post type to the edit
+      'all_items' => 'All Campuses',
+      'singular_name' => 'Campuses',
+      'new_item' => 'New Campus',
+      'view_item' => 'View Campus',
+      'search_items' => 'Search in Campuses',
+      'not_found' => 'No Campus found',
+      'not_found_in_trash' => 'No Campus found in Trash',
+      'parent_item_colon' => ''
+    )
+  ));
   // event post type
   register_post_type('event', array(
     'show_in_rest' => 'true', // to use the modern block editor in wp
@@ -77,4 +104,3 @@ function university_post_types()
   );
 };
 add_action('init', 'university_post_types');
-
